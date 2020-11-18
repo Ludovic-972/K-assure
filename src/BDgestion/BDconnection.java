8b5ba252e409c93a939773edeeaa50995a4f7187
Ludovic-972 <ludovicmathurin1@gmail.com>
2020-11-18 04:48:09 +0100

    Ajout de la JavaDoc dans les classes du package BDgestion

--- src/BDgestion/BDconnection.java
+++ src/BDgestion/BDconnection.java
@@ -1,45 +1,79 @@
 package BDgestion;
 import java.sql.*;
 
+/**
+ * <b>BDconnection est la classe utilisée pour créer une connexion à la base de données<b>
+ * <p>
+ * Elle contient une instance de la classe Connection qui réalise 
+ * la connexion et l'authentification à la base de données. 
+ * </p>
+ * <p>
+ * De plus cette classe permet d'envoyer les reqûetes SQL à la base de données 
+ * pour les exécuter et occasionnellement renvoyer les données dans un ResultSet.
+ * </p>
+ * @author Ludovic Mathurin-Cayol
+ * @version 2.0
+ * */
 
 public class BDconnection{
 	
+	/**
+	 * 
+	 * */
 	private Connection connection;
 	
+	/**
+	 * Constructeur de la connexion à la base de données
+	 * <p>
+	 *A la construction d'un objet BDconnection une connexion et une authentification
+	 *à la base de données est créée par le biais d'une instance de la classe Connection. 
+	 *</p>
+	 *<p>
+	 *La connexion se fait d'abord par le chargement du pilote JDBC puis
+	 *par une instance de la classe Driver et enfin l'authentification 
+	 *à la base grâce à l'url de celle-ci,le login de l'utilisateur et son mot de passe.
+	 *</p>
+	 *
+	 *@throws SQLException ou ClassNotFoundException en cas d'inexistant de la classe 
+	 *			ou d'erreur d'authentification à la base.
+	 *
+	 *@see BDconnection#connection 
+	 *
+	 *@since 1.0
+	 * */
 	public BDconnection() {
 		String nom_driver = "com.mysql.cj.jdbc.Driver";
 		String url = "jdbc:mysql://mysql-mathurin.alwaysdata.net:3306/mathurin_k_assure"  // Lien vers la base de données
 				+ "?serverTimezone=UTC"; // Fuseau horaire (UTC = Universal Time Coordinated)
 		String login = "mathurin";
 		String pwd = "85#Rc$4wM";
-		connection = null;
+		this.connection = null;
 		try {
 			/* Connexion avec la base */
-			Class.forName(nom_driver);
-			connection = DriverManager.getConnection(url,login,pwd);
+			Class.forName(nom_driver); // Chargement du pilote JDBC + instance de la classe Driver
+			this.connection = DriverManager.getConnection(url,login,pwd);  // Connexion à la base de données
 		} catch (SQLException | ClassNotFoundException e) {
 			System.out.println("Connexion to the database denied."); 
 		}
 		
 	}
 	
-	public Connection getConnection() {
-		return connection;
-	}
-	
-	public void close() {
-		try {
-			connection.close();
-		}catch(SQLException e) {
-			
-		}
-	}
-	
+	/**
+	 *Retourne le nombre de lignes d'un ResultSet
+	 *
+	 *@param rs : 
+	 *		Instance de ResultSet traité
+	 *
+	 *@return Le nombre de ligne de rs 
+	 *
+	 *@since 1.0
+	 * 
+	 */
 	public int getRowCount(ResultSet rs) {
 		int a = 0;
 		try {
 			while (rs.next()){
-				a = rs.getInt(1); //typeID is the number of rows in the ResultSet
+				a++;
 			}
 		 return a;
 		} catch (SQLException e) {
@@ -49,6 +83,17 @@ public class BDconnection{
 		return 0;
 	}
 	
+	/**
+	 *Retourne le nombre de colonnes d'un ResultSet
+	 *
+	 *@param rs : 
+	 *		Instance de ResultSet traité
+	 *
+	 *@return Le nombre de colonne de rs 
+	 *
+	 *@since 1.0
+	 * 
+	 */
 	public int getColomnCount(ResultSet rs) {
 		try {
 			return rs.getMetaData().getColumnCount();
@@ -58,30 +103,25 @@ public class BDconnection{
 		return 0;
 	}
 	
-	public void affiche(String req) {
-		try {
-			testRequete(req);
-			try {
-				Statement st = connection.createStatement();
-				System.out.println("Statement created.");
-				
-				//Selection 
-				ResultSet rs = st.executeQuery(req);
-				while (rs.next()) {
-					   for (int i = 1; i <= getColomnCount(rs); i++) {
-						   String a = rs.getString(i);
-						   System.out.print(a+" | ");
-					   }
-					   System.out.println();
-					}
-			} catch (SQLException e) {
-				System.out.println("Problème d'affichage.");
-			}
-		} catch (RequeteNull e1) {
-			e1.printStackTrace();
-		}
-	}
-
+	/**
+	 *<p>
+	 *Retourne un ResultSet contenant les données renvoyées
+	 *par la bases de données grâce à une requête SQL de type <i>SELECTs</i>.
+	 *Le ResultSet a deux arguments:
+	 *</p>
+	 *<ul>
+	 *	<li> TYPE_SCROLL_INSENSITIVE : ResultSet insensible aux modifications des valeurs dans la base de données.
+	 *								   Vue statique des données du ResultSet.
+	 *	<li> CONCUR_READ_ONLY: Indique que les données du ResultSet ne peuvent qu'être lues;
+	 *</ul>
+	 *
+	 *
+	 *@param req :Requête SQL envoyé à la base de donnée grâce à un Statement.
+	 *
+	 *@return Les données d'une requête SQL
+	 *
+	 *@since 1.0
+	 */
 	public ResultSet getResult(String req) {
 		Statement st = null;
 		ResultSet rs = null;
@@ -104,6 +144,23 @@ public class BDconnection{
 
 	}
 	
+	
+	/**
+	 * <p>
+	 * Exécute la requête demandée généralement utilisée pour les <i>INSERTs<i> ou les <i>UPDATEs<i>.
+	 * Le ResultSet défini dans la création du Statement a deux arguments:
+	 *</p>
+	 *<ul>
+	 *	<li> TYPE_SCROLL_INSENSITIVE : ResultSet insensible aux modifications des valeurs dans la base de données.
+	 *								   Vue statique des données du ResultSet.
+	 *	<li> CONCUR_READ_ONLY: Indique que les données du ResultSet ne peuvent qu'être lues;
+	 *</ul>
+	 * 
+	 * @param req: Requête SQL envoyé à la base de donnée grâce à un Statement.
+	 * 
+	 * @since 1.0
+	 * 
+	 * */
 	public void executeQuery(String req) {
 		try {
 			testRequete(req);
@@ -119,7 +176,15 @@ public class BDconnection{
 			e1.printStackTrace();
 		}
 	}
-
+	
+	/**
+	 * Teste si une requête est null
+	 * 
+	 * @param s: requête SQL dont la valeur sera évalué
+	 * 
+	 * @see RequeteNull
+	 * @since 2.0
+	 * */
 	public void testRequete(String s)throws RequeteNull {
 		if(s.equals(null))
 			throw new RequeteNull("Requete nulle.");
--- src/BDgestion/RequeteNull.java
+++ src/BDgestion/RequeteNull.java
@@ -1,9 +1,41 @@
 package BDgestion;
 
+/**
+ * <p>
+ * Classe héritant de la classe Exception qui lance une exception
+ * quand une requête SQL est null.
+ * </p>
+ * 
+ * @see BDconnection#testRequete(String)
+ * 
+ * @author Ludovic Mathurin-Cayol
+ * @version 1.0
+ * */
 @SuppressWarnings("serial")
 public class RequeteNull extends Exception{
 	
+	/**
+	 * <p>
+	 * Constructeur avec en paramètre une chaine de caractère.
+	 * </p>
+	 * 
+	 * @param s : 
+	 * 			Chaine de caractère qui définit le message qui décrit l'exception.
+	 * 
+	 * @since 1.0
+	 */
 	public RequeteNull(String s) {
 		super(s);
+	}
+	
+	/**
+	 * <p>
+	 * Constructeur sans paramètre.
+	 * </p>
+	 * 
+	 * @since 1.0
+	 */
+	public RequeteNull() {
+		super();
 	}
 }
