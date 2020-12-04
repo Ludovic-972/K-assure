9e2a8338c85fbaefb438011fb0e2c93f9bf8f2a5
ShayeEyahs <74192753+ShayeEyahs@users.noreply.github.com>
2020-12-04 01:52:02 +0100

    Add files via upload
    Fichier PageDeConnection/PageAdmin/PageAccueil améliorer, Fichier Sante correspond au formulaire pour une assurance sante renvoie NumberOfFamily/Type de contrat/Tranche d'age de chaque individue.

---
+++ NouveauxContrat.java
@@ -0,0 +1,96 @@
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+public class NouveauxContrat extends JFrame implements ItemListener {
+
+	
+	private static final long serialVersionUID = 1L;
+	JPanel fenetre = new JPanel();
+	public JPanel panel;
+	public JPanel panel2;
+	public JComboBox contrat;
+	public JFrame fenetre1;
+			
+	public NouveauxContrat() {
+		fenetre1 = new JFrame();
+		fenetre1.setLayout(new BorderLayout());
+		fenetre1.setSize(500,500);
+		fenetre1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		fenetre1.setTitle("formulaire");
+		fenetre1.setLocation(1150,320);
+		fenetre1.setResizable(true);
+		
+		
+		contrat = new JComboBox();
+		contrat.addItem(" ");
+		contrat.addItem("habitation");
+		contrat.addItem("santé");
+		contrat.addItem("véhicule");
+		contrat.addItem("scolaire");
+		contrat.addItemListener(this);
+		
+		 
+		panel = new JPanel();
+		panel.setLayout(new FlowLayout());
+		panel2 = new JPanel();
+		panel2.setLayout(new FlowLayout());
+		panel.add(contrat);
+		fenetre1.add(panel, BorderLayout.NORTH);
+		fenetre1.add(panel2, BorderLayout.CENTER);
+		fenetre1.setVisible(true);
+		
+		}
+	
+	public void itemStateChanged(ItemEvent item) {
+		if ( item.getItem() == "habitation") {
+			habitation hab = new habitation();
+			hab.setVisible(true);			
+			fenetre1.setVisible(false);
+
+		}
+		else if (item.getItem() == "scolaire") {
+				scolaire sco = new scolaire();
+				sco.setVisible(true);
+				fenetre1.setVisible(false);
+					
+			
+		}else if (item.getItem() == "santé") {
+			
+			Sante sante = new Sante(item.getItem());
+			sante.setVisible(true);
+			fenetre1.setVisible(false);
+				
+		
+	}else if (item.getItem() == "véhicule") {
+		véhicule veh = new véhicule();
+		veh.setVisible(true);
+		fenetre1.setVisible(false);
+			
+	
+}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
