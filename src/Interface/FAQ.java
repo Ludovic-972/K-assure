d8c30fcd50e536ceb8a8f723ebf3aa9ad22d4cc5
Ludovic-972 <ludovicmathurin1@gmail.com>
2020-12-13 22:57:57 +0100

    Modification FAQ

--- src/Interface/FAQ.java
+++ src/Interface/FAQ.java
@@ -1,6 +1,7 @@
 package Interface;
 
 
+import java.awt.Font;
 import java.awt.GridLayout;
 
 import javax.swing.JFrame;
@@ -25,18 +26,17 @@ public class FAQ  extends JFrame{
 	 */
 	public FAQ() {
 		
-		panel = new JPanel();
 
-		JLabel q1 = new JLabel("Q : Comment déclarer un sinistre ?");
-		JLabel q2 = new JLabel("Q : Comment puis-je contacter mon assurance ?");
-		JLabel q3 = new JLabel("Q : Mon animal est-il couvert par mon assurance habitation ?");
-		JLabel q4 = new JLabel("Q : Peut-on assurer des appartements ?");
-		JLabel q5 = new JLabel("Q : Assurez-vous les deux roues ?");
-		JLabel q6 = new JLabel("Q : Est-ce que je peux assurer mon vélo ?");
-		JLabel q7 = new JLabel("Q : Les frais d’ostéopathe ou de chiropracteur peuvent-ils être remboursés ?");	
-		JLabel q8 = new JLabel("Q : Les frais d'amniocentèse non remboursés par la Sécurité sociale peuvent-ils être pris en charge ?");	
-		JLabel q9 = new JLabel("Q : Puis-je avoir l'assurance scolaire si je suis étudiant ?");
-		JLabel q10 = new JLabel("Q : Est-ce que je vais avoir besoin de prendre une nouvelle assurance scolaire quand je vais changer d'établissement ?");
+		JLabel q1 = new JLabel("<html><body><u>Q : Comment déclarer un sinistre ?</u></body></html>");
+		JLabel q2 = new JLabel("<html><body><u>Q : Comment puis-je contacter mon assurance ?</u></body></html>");
+		JLabel q3 = new JLabel("<html><body><u>Q : Mon animal est-il couvert par mon assurance habitation ?</u></body></html>");
+		JLabel q4 = new JLabel("<html><body><u>Q : Peut-on assurer des appartements ?</u></body></html>");
+		JLabel q5 = new JLabel("<html><body><u>Q : Assurez-vous les deux roues ?</u></body></html>");
+		JLabel q6 = new JLabel("<html><body><u>Q : Est-ce que je peux assurer mon vélo ?</u></body></html>");
+		JLabel q7 = new JLabel("<html><body><u>Q : Les frais d’ostéopathe ou de chiropracteur peuvent-ils être remboursés ?</u></body></html>");	
+		JLabel q8 = new JLabel("<html><body><u>Q : Les frais d'amniocentèse non remboursés par la Sécurité sociale peuvent-ils être pris en charge ?</u></body></html>");	
+		JLabel q9 = new JLabel("<html><body><u>Q : Puis-je avoir l'assurance scolaire si je suis étudiant ?</u></body></html>");
+		JLabel q10 = new JLabel("<html><body><u>Q : Est-ce que je vais avoir besoin de prendre une nouvelle assurance scolaire quand je vais changer d'établissement ?</u></body></html>");
 	
 		JLabel r1 = new JLabel("R : Pour déclarer un sinistre, il suffit d'aller dans la section \"Déclarer un sinistre\".");
 		JLabel r2 = new JLabel("R : Pour pouvoir contacter votre assurance, vous devez aller dans la rubrique \"Contact\".");
@@ -49,42 +49,37 @@ public class FAQ  extends JFrame{
 		JLabel r9 = new JLabel("R : Non, il faut que vous soyez scolarisé dans une école maternelle, primaire, collège ou lycée.");
 		JLabel r10 = new JLabel("R : Pas besoin, votre assurance scolaire vous suivra jusqu'à l'obtention de votre baccalauréat.");
 		
+		panel = new JPanel(new GridLayout(10, 1));
+		JLabel[] q = {q1,q2,q3,q4,q5,q6,q7,q8,q9,q10};
+		JLabel[] r = {r1,r2,r3,r4,r5,r6,r7,r8,r9,r10};
+		
+		JPanel[] tab = new JPanel[10];
+		
+		for (int i = 0; i < tab.length; i++) {
+			tab[i] = new JPanel(new GridLayout(2,1));
+			
+			q[i].setFont(new Font("Arial",Font.TYPE1_FONT, 14));
+			r[i].setFont(new Font("Arial",Font.TYPE1_FONT, 13));
+			
+			tab[i].add(q[i]);
+			tab[i].add(r[i]);
+			panel.add(tab[i]);
+		}
 		
 		
-		panel.add(q1);
-		panel.add(r1);
-		panel.add(q2);
-		panel.add(r2);
 		
-		panel.add(q3);
-		panel.add(r3);
-		panel.add(q4);
-		panel.add(r4);
-		
-		panel.add(q5);
-		panel.add(r5);
-		panel.add(q6);
-		panel.add(r6);
-		
-		panel.add(q7);
-		panel.add(r7);
-		panel.add(q8);
-		panel.add(r8);
-		
-		panel.add(q9);
-		panel.add(r9);
-		panel.add(q10);
-		panel.add(r10);
-		
-		panel.setLayout(new GridLayout(0, 2, -400, 20));
-		
-		add(panel);
+
+		this.add(panel);
 	
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		this.setTitle("FAQ");       
-		this.pack();
+		this.setSize(1300,500);
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new FAQ();
 	}
 }
 
